3ac220e867f3fcae98c4ed8747dbb981f18cdbbc
peiyu <peiyu@canlead.com.tw>
2025-06-19 20:23:16 +0800

    detail error test

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,13 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
 
+function addToArray(value, array = []) {
+    array.push(value);
+    return array;
+}
+
+const result1 = addToArray(1);
+const result2 = addToArray(2);
+
+console.log(result1); // [1, 2]
+console.log(result2); // [1, 2]
