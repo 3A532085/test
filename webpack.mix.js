aee3b5cf338fe4e62a3e696a038c61f4315c8541
peiyu <peiyu@canlead.com.tw>
2025-06-20 17:12:52 +0800

    function GetUser

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,5 +18,18 @@ var Data = require('axios');
 
 
 
+function GetUser(d) {
+    for (var i = 0; i <= d.length; i++) {
+        if (d[i] != null && d[i] != undefined) {
+            console.log("user id is: " + d[i].id);
+        }
+    }
+
+    Data.get("https://api.example.com/data").then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.log("error happened");
+    });
+}
 
 
